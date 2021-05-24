f0afb9ea20858dfca3ddf81ec36e4ebcb647cbfd
DMarcelo <david.saenz8818@est.uca.edu.ni>
2021-05-24 11:22:36 -0600

    Realization of di, intent, model, repository, retrofit, room and utils

--- app/src/main/java/com/scrummaster/arboretocarmelopalma/ui/MainActivity.kt
+++ app/src/main/java/com/scrummaster/arboretocarmelopalma/ui/MainActivity.kt
@@ -3,7 +3,12 @@ package com.scrummaster.arboretocarmelopalma.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.scrummaster.arboretocarmelopalma.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
